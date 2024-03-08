8f7221875626973ead100637d8b42789acf81458
parvinder <parvinderbabar@gmail.com>
2024-03-07 19:53:04 -0500

    commit

--- intro-react/src/app/page.jsx
+++ intro-react/src/app/page.jsx
@@ -1,20 +1,45 @@
 "use client"
-import Image from "next/image";
+// import Image from "next/image";
 import { useState } from "react"; 
 import alienPlanets from "../lib/alien-planets.jsx";
+import Divider from "@/component/Divider.jsx";
+import memeImage from "../../public/meme.webp";
+
 
 export default function Home() {
-  const[planets,setPlanets]= useState(alienPlanets);
+  const [planets, setPlanets] = useState(alienPlanets);
 
-return (
-   
-  <div id="main">
-   
+  return (
+    <div>
+      {<p>1</p>}
+      <Divider />
+
+      {<p>2</p>}
+       <div id="square">
+        {
+          
+        }
+        </div>
+      <Divider />
+       
+    
+      
+      {<p>3</p>}
+       {/* <Image src={memeImage} alt="Meme" /> */}
+
+      {<div id="image-meme">
+         <img src="/meme.webp" alt="img" />
+     </div>}
+      <Divider/>
+      <h3 > Alien planets</h3>
+      
+    <div id="main">
+  
       {
         planets.map((planet) => {
           return (<div >
             <div key={planet.id} className="planet-container">
-              <h6> {planet.emoji}</h6>
+              <h2> {planet.emoji}</h2>
             
             
     
@@ -25,11 +50,13 @@ return (
               </p>
             </div>
           </div>);
-              // Zorbon is 320 light years miles from earth.Zorbon is believed to have life. 
-              // The atmosphere of Zorbon is nitrogen - oxygen.
+          // Zorbon is 320 light years miles from earth.Zorbon is believed to have life. 
+          // The atmosphere of Zorbon is nitrogen - oxygen.
            
         })}
-</div>);
+    </div>
+      </div >);
+
 }
 //  const [dragons, setDragons] = useState(defaultDragons);
 //   console.log(defaultDragons);
